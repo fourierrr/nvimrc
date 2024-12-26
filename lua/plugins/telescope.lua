80c3cf168aea769b673796313a5b107428893b08
fourierrr <fourierrr@gmail.com>
2024-12-26 12:42:45 +0800

    feat: add hop.nvim and symbols-outline.nvim fix: disabled session manger persistence.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,7 +3,15 @@ return {
   -- replace all Telescope keymaps with only one mapping
   keys = function()
     return {
-      { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      -- { "<leader>ff", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      -- { "<leader>fF", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      -- { "<leader>sg", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+      -- { "<leader>sG", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
+      --
+      -- { "<leader>f", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
+      -- { "<leader>F", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+      -- { "<leader>t", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
+      -- { "<leader>T", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
     }
   end,
 }
